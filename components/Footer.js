350adba5958e1413f00c503a33e85cce739f4058
Bruce Owenga <bruceowenga@gmail.com>
2022-01-06 18:41:59 +0300

    initial commit

---
+++ components/Footer.js
@@ -0,0 +1,41 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md: grid-cols-4 gap-y-10 px-32 py-14 bg-gray-400 text-gray-600">
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How AirBnb Works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>AirBnb Plus</p>
+                <p>AirBnb Luxe</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">COMMUNITY</h5>
+                    <p>Accessibility</p>
+                    <p>This is not a real site</p>
+                    <p>It's a pretty awesome clone</p>
+                    <p>Referrals accepted</p>
+                    <p>Ask Bruce</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                    <p>Bruce Mbudi</p>
+                    <p>Presents</p>
+                    <p>A React Project</p>
+                    <p>With Next.js</p>
+                    <p>And Tailwind CSS</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">SUPPORT</h5>
+                    <p>Feel free to contact me</p>
+                    <p>For projets</p>
+                    <p>Of any scale</p>
+                    <p>See you soon</p>
+                    <p>Cheers</p>
+            </div>
+        </div>
+
+    );
+}
+
+export default Footer
